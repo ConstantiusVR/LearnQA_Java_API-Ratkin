380c97fa02bf716bd2118d23a82a123d48bf10b2
Constantine Ratkin <r-const@yandex.ru>
2024-02-14 14:02:43 +0300

    Hello from me

---
+++ src/test/java/HelloProgram.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloProgram {
+
+    @Test
+    public void testHelloFromMe(){
+        System.out.println("Hello from Constantin!");
+    }
+
+}
