cbd4af41fa2fd8a7bc281e59540dc5c6c157e891
Constantine Ratkin <r-const@yandex.ru>
2024-02-22 16:28:04 +0300

    Homework 2 task 7

--- src/test/java/TestLongRedirection.java
+++ src/test/java/TestLongRedirection.java
@@ -10,43 +10,38 @@ public class TestLongRedirection {
 
     @Test
 
-    public void repFindLocationLong(String x) {
-        Response response = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .when()
-                .get(x)
-                .andReturn();
-        String locationHeader = response.getHeader("Location");
-
-        int statusCode = response.getStatusCode();
-        System.out.println(statusCode);
-        x = locationHeader;
-        System.out.println(x);
-    }
-
+// https://playground.learnqa.ru/api/long_redirect
+//    https://playground.learnqa.ru/
+//   https://learnqa.ru/
+//    https://www.learnqa.ru/
 
     public void findLocationLong() {
-        Response response = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .when()
-                .get("https://playground.learnqa.ru/api/long_redirect")
-                .andReturn();
-        String locationHeader = response.getHeader("Location");
-        System.out.println(locationHeader);
-        int statusCode = response.getStatusCode();
-        System.out.println(statusCode);
+        String adUrl = "https://playground.learnqa.ru/api/long_redirect";
 
+        Response response;
         do {
-            repFindLocationLong(locationHeader);
+            response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(adUrl)
+                    .andReturn();
+            String locationHeader = response.getHeader("Location");
+            System.out.println(locationHeader);
+            int statusCode = response.getStatusCode();
+            System.out.println(statusCode);
+            adUrl = locationHeader;
         } while (response.getStatusCode() == 301);
+
 
     }
 
-}
+
+
+    }
+
+
 
 
 
