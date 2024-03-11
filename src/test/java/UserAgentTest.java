f6f5e678c57e263824ecc9cc3c70b3a7f4202dca
Constantine Ratkin <r-const@yandex.ru>
2024-03-11 16:06:18 +0300

    Homework 3 task 13

---
+++ src/test/java/UserAgentTest.java
@@ -0,0 +1,115 @@
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class UserAgentTest {
+
+    @ParameterizedTest
+    @ValueSource (strings = {"Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+    "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+    "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"})
+
+    public void testAgent(String name) {
+
+
+Map<String, String> headers = new HashMap<>();
+headers.put("user-agent", name);
+
+JsonPath response1 = RestAssured
+            .given()
+            .headers(headers)
+            .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+            .jsonPath();
+
+    String platform1 = response1.getString("platform");
+    String browser1 = response1.getString("browser");
+    String device1 = response1.getString("device");
+    String answer1 = "platform" + ": " + platform1 + ", " + "browser" + ": " + browser1 + ", " + "device" + ": " + device1;
+
+
+       String expectedValue1 = "platform: Mobile, browser: No, device: Android";
+       assertEquals(expectedValue1, answer1, "Unexpected values");
+
+
+
+JsonPath response2 = RestAssured
+                .given()
+                .headers(headers)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+
+        String platform2 = response2.getString("platform");
+        String browser2 = response2.getString("browser");
+        String device2 = response2.getString("device");
+        String answer2 = "platform" + ": " + platform2 + ", " + "browser" + ": " + browser2 + ", " + "device" + ": " + device2;
+
+
+        String expectedValue2 = "platform: Mobile, browser: Chrome, device: iOS";
+        assertEquals(expectedValue2, answer2, "Unexpected values");
+
+
+
+
+JsonPath response3 = RestAssured
+                .given()
+                .headers(headers)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+
+        String platform3 = response3.getString("platform");
+        String browser3 = response3.getString("browser");
+        String device3 = response3.getString("device");
+        String answer3 = "platform" + ": " + platform3 + ", " + "browser" + ": " + browser3 + ", " + "device" + ": " + device3;
+
+
+        String expectedValue3 = "platform: Googlebot, browser: Unknown, device: Unknown";
+        assertEquals(expectedValue3, answer3, "Unexpected values");
+
+
+
+
+JsonPath response4 = RestAssured
+                .given()
+                .headers(headers)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+
+        String platform4 = response4.getString("platform");
+        String browser4 = response4.getString("browser");
+        String device4 = response4.getString("device");
+        String answer4 = "platform" + ": " + platform4 + ", " + "browser" + ": " + browser4 + ", " + "device" + ": " + device4;
+
+
+        String expectedValue4 = "platform: Web, browser: Chrome, device: No";
+        assertEquals(expectedValue4, answer4, "Unexpected values");
+
+
+
+JsonPath response5 = RestAssured
+                .given()
+                .headers(headers)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+
+        String platform5 = response5.getString("platform");
+        String browser5 = response5.getString("browser");
+        String device5 = response5.getString("device");
+        String answer5 = "platform" + ": " + platform5 + ", " + "browser" + ": " + browser5 + ", " + "device" + ": " + device5;
+
+
+        String expectedValue5 = "platform: Mobile, browser: No, device: iPhone";
+        assertEquals(expectedValue5, answer5, "Unexpected values");
+
+
+
+    }
+
+}
